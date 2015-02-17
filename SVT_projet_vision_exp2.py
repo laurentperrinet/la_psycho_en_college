corrected units / flip text

SVT_projet_vision_exp2.py
@@ -19,6 +19,14 @@ info['screen_width'] = 51.5
 info['screen_distance'] = 57.
 info['nTrials'] = 50
 
+N_taille, taille_0 = 5, 1.
+N_ecc = 3
+core_wait = 0.100
+core_wait_stim = 0.300
+
+eccen_ = np.linspace(5., 15., N_ecc, endpoint=True) # en degrés d'angle visuel
+taille_ = np.logspace(-1.5, 1.5, N_taille, endpoint=True, base=2) * taille_0  # en degrés d'angle visuel
+
 try:
     dlg = gui.DlgFromDict(info)
 except:
@@ -27,7 +35,7 @@ except:
 
 import time
 info['timeStr'] = time.strftime("%b_%d_%H%M", time.localtime())
-fileName = 'data/' + experiment + info['observer'] + '_' + info['timeStr'] + '.pickle'
+fileName = 'data/' + experiment + '_' +  + info['observer'] + '_' + info['timeStr'] + '.pickle'
 #save to a file for future use (ie storing as defaults)
 if dlg.OK:
     misc.toFile(fileName, info)
@@ -53,10 +61,10 @@ Pressez sur une de ces 2 touches pour continuer...
 
 # Objets correspondant à la croix de fixation, à la consigne de réponse et aux instructions:
 wait_for_next = visual.TextStim(win, 
-                        text = u"+", units='norm', height=0.05, color='BlanchedAlmond',
+                        text = u"+", units='norm', height=0.15, color='white',
                         pos=[0., -0.], alignHoriz='center', alignVert='center' ) 
 wait_for_response = visual.TextStim(win, 
-                        text = u"?", units='norm', height=0.05, color='DarkSlateBlue',
+                        text = u"?", units='norm', height=0.15, color='DarkSlateBlue',
                         pos=[0., -0.], alignHoriz='center', alignVert='center' ) 
 instructions_txt = visual.TextStim(win, 
                         text = instructions, units='norm', height=0.05, color='BlanchedAlmond',
@@ -80,6 +88,7 @@ def getResponse():
                 visual.TextStim(win, "pressez < ou > (ou Esc pour sortir) (mais pas %s)" %key, height=0.05, color='red').draw()
                 win.flip()
 
+# http://www.psychopy.org/general/units.html
 def angle2cm(size_in_deg, screen_distance):
     return 2* screen_distance * np.tan(size_in_deg * np.pi / 180. / 2.)
 
@@ -91,16 +100,8 @@ def presentStimulus(consigne, eccen, taille):
     """
     stim = visual.TextStim(win, text=u"E", units='norm', height=angle2norm(taille, info['screen_distance'], info['screen_width']), color='black',
                         pos=[angle2norm(eccen, info['screen_distance'], info['screen_width']), 0], alignHoriz='center', alignVert='center',
-                        flip=not(consigne))
+                        flipHoriz=not(consigne))
     stim.draw()
-
-# http://www.psychopy.org/general/units.html
-N_ecc = 3
-eccen_ = np.linspace(5., 15., N_ecc, endpoint=True) # en degrés d'angle visuel
-N_taille = 5
-taille_ = np.logspace(-1, 1., N_taille, endpoint=True, base=10) * 2.  # en degrés d'angle visuel
-core_wait = 0.100
-core_wait_stim = 0.300
 
 # initialisation: on montre les instructions
 instructions_txt.draw()
@@ -117,6 +118,7 @@ for i_trial in range(info['nTrials']):
     flip = np.random.randint(2)*2-1
     eccen = flip*eccen_[np.random.randint(N_ecc)]
     taille = taille_[np.random.randint(N_taille)]
+    wait_for_next.draw()
     presentStimulus(consigne, eccen, taille)
     win.flip()
     core.wait(core_wait_stim)
@@ -134,5 +136,5 @@ win.close()
 
 print results
 #save data
-fileName = 'data/' + experiment + info['observer'] + '_' + info['timeStr']
+fileName = 'data/' + experiment + '_' +  + info['observer'] + '_' + info['timeStr']
 np.save(fileName, results)