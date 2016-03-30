6c968f5be30df3c35b53160ff4f62b8c4c88c0db
tony2693 <tony_wanping@hotmail.com>
2016-03-30 11:02:50 +0200

    ajout e commentaies

--- fle.py
+++ fle.py
@@ -1,147 +1,149 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Flash_lag Effect
+
+
+
+
+"""
+
 from psychopy import visual, event, core, logging, gui, misc
 import time
 import numpy as np
 
+# paramètres par défault
+w, h = 2560, 1440 # iMac 27''
+w, h = 1920, 1200 # cinema display
+w, h = 1366, 768 # anthony
+
+#if no file use some defaults
+info = {}
+info['datapath'] = 'data' 
+info['observer'] = 'anonymous'
+info['screen_width'] = w
+info['screen_height'] = h
+info['nTrials'] = 20
+info['duration'] = 1. #duration of the stimulus in seconds
+info['radius'] = .008
+info['step'] = .02
+info['pos_flash'] = .5
+info['std_flash'] = .05
+info['noise'] = .05
+info['length'] = .5 # length of the trajectory, 1 is the height of the window
+
+experiment = 'fle'
+
 try:
-    fileName = 'data/2016-03-03_fle_anonymous'
-    info = misc.fromFile(fileName + '.pickle')
-    results = np.load(fileName + '.npy')
-
+    dlg = gui.DlgFromDict(info)
 except:
-    w, h = 2560, 1440 # iMac 27''
-    w, h = 1920, 1200 # cinema display
+    print('Could not load gui... running with defaut parameters')
+    print(info)
 
-    #if no file use some defaults
-    info = {}
-    info['observer'] = 'anonymous'
-    info['screen_width'] = w
-    info['screen_height'] = h
-    info['nTrials'] = 30
-    info['duration'] = 1. #duration of the stimulus in seconds
-    info['radius'] = .006
-    info['step'] = .02
-    info['pos_flash'] = .5
-    info['std_flash'] = .05
-    info['noise'] = .05
-    info['length'] = .5 # length of the trajectory, 1 is the height of the window
-
-    experiment = 'fle'
-
-    try:
-        dlg = gui.DlgFromDict(info)
-    except:
-        print('Could not load gui... running with defaut parameters')
-        print(info)
-        
-    info['timeStr'] = time.strftime("%Y-%m-%d_%H%M%S", time.localtime())
-    fileName = 'data/' + info['timeStr'] + '_' + experiment + '_' + info['observer'] + '.pickle'
-    #save to a file for future use (ie storing as defaults)
-    if dlg.OK:
-        misc.toFile(fileName, info)
-    else:
-        print('Interrupted gui... quitting')
-        core.quit() #user cancelled. quit
+import os
+try:
+    os.mkdir(info['datapath'])
+except:
+    pass
 
 
-    win = visual.Window([info['screen_width'], info['screen_height']], fullscr=True, units='height')
-
-    myMouse = event.Mouse()  #  will use win by default
-
-    stim = visual.Circle(win, units='height', 
-            radius = info['radius'],
-            interpolate = True, fillColor='Navy',
-            autoLog=False)#this stim changes too much for autologging to be useful
-
-    flash = visual.Circle(win, units='height', 
-            radius = info['radius'],
-            interpolate = True, fillColor='Red',
-            autoLog=True)#this stim changes too much for autologging to be useful
-
-    fixation = visual.Circle(win, units='height', 
-            pos=[0., 0.3], radius = 0.03,
-            interpolate = True, fillColor='BlanchedAlmond',
-            autoLog=True)#this stim changes too much for autologging to be useful
+info['timeStr'] = 'debug' # time.strftime("%Y-%m-%d_%H%M%S", time.localtime())
+fileName = os.path.join(info['datapath'], info['timeStr'] + '_' + experiment + '_' + info['observer'] + '.pickle')
+#save to a file for future use (ie storing as defaults)
+if dlg.OK:
+    misc.toFile(fileName, info)
+else:
+    print('Interrupted gui... quitting')
+    core.quit() #user cancelled. quit
 
 
-    wait_for_response = visual.TextStim(win, 
-                            text = u"?", units='norm', height=0.15, color='DarkSlateBlue',
-                            pos=[0., -0.], alignHoriz='center', alignVert='center' ) 
-    fixation_t = visual.TextStim(win, 
-                            text = u"+", units='norm', height=0.15, color='BlanchedAlmond',
-                            pos=[0., 0.3], alignHoriz='center', alignVert='center' ) 
-                            
-    def getResponse():
-        #myMouse.setPos((0, 0))
-        myMouse.setVisible(1)
-        event.clearEvents()#clear the event buffer to start with
-        resp = None#initially
-        while 1:#forever until we return mouse events
-            mouse1, mouse2, mouse3 = myMouse.getPressed()
-            if (mouse1):
-                return myMouse.getRel()
-            elif mouse3 or mouse2:
-                win.close()
-                core.quit()
-                return None 
+win = visual.Window([info['screen_width'], info['screen_height']], fullscr=True, units='height')
 
-    clock = core.Clock()
-    def presentStimulus(pos_flash, noise_flash):
-        """Present stimulus
-        """
-        clock.reset()
-        myMouse.setVisible(0)
-        did_flash = False
-        while clock.getTime() < info['duration']:
-            fixation.draw()
-            stim.setPos(( info['length']*(np.float(clock.getTime()/info['duration'])-.5), info['step']/2))
-            stim.draw()
-            if not(did_flash) and (clock.getTime() / info['duration']) > pos_flash:
-                flash.setPos(( info['length']*(np.float(clock.getTime()/info['duration'])-.5) + noise_flash, -info['step']/2))
-                flash.draw()
-                did_flash = True
-                #print clock.getTime(), np.float(clock.getTime()/info['duration'])-.5, stim.pos
-            win.flip()
+myMouse = event.Mouse()  #  will use win by default
 
-    results = np.zeros((4, info['nTrials']))
-    for i_trial in range(info['nTrials']):
+#On construit les differents cercles et l'arriere plan.
+stim = visual.Circle(win, units='height', 
+        radius = info['radius'],
+        interpolate = True, fillColor='Navy',
+        autoLog=False)#this stim changes too much for autologging to be useful
+
+flash = visual.Circle(win, units='height', 
+        radius = info['radius'],
+        interpolate = True, fillColor='Red',
+        autoLog=True)#this stim changes too much for autologging to be useful
+
+fixation = visual.Circle(win, units='height', 
+        pos=[0., 0.3], radius = 0.03,
+        interpolate = True, fillColor='BlanchedAlmond',
+        autoLog=True)#this stim changes too much for autologging to be useful
+
+wait_for_response = visual.TextStim(win, 
+                        text = u"?", units='norm', height=0.15, color='DarkSlateBlue',
+                        pos=[0., -0.], alignHoriz='center', alignVert='center' ) 
+fixation_t = visual.TextStim(win, 
+                        text = u"+", units='norm', height=0.15, color='BlanchedAlmond',
+                        pos=[0., 0.3], alignHoriz='center', alignVert='center' ) 
+                        
+                        
+#Tant que la souris est visible il y a pas de stimuli sur l'ecran.
+#Une fois stimuli lance on enregistre la nouvelle position de la souris.
+def getResponse():
+    #myMouse.setPos((0, 0))
+    myMouse.setVisible(1)
+    event.clearEvents()#clear the event buffer to start with
+    resp = None#initially
+    while 1:#forever until we return mouse events
+        mouse1, mouse2, mouse3 = myMouse.getPressed()
+        if (mouse1):
+            return myMouse.getRel()
+
+#Quand le stimulus bleu mobile apparait la souris disparait. 
+#Tant que le stimulus present est inferieur a "duration" le flash rouge n'apparait pas, sinon il apparait.
+clock = core.Clock()
+def presentStimulus(pos_flash, noise_flash):
+    """Present stimulus
+    """
+    clock.reset()
+    myMouse.setVisible(0)
+    did_flash = False
+    while clock.getTime() < info['duration']:
         fixation.draw()
+        stim.setPos(( info['length']*(np.float(clock.getTime()/info['duration'])-.5), info['step']/2))
+        stim.draw()
+        if not(did_flash) and (clock.getTime() / info['duration']) > pos_flash:
+            flash.setPos(( info['length']*(np.float(clock.getTime()/info['duration'])-.5) + noise_flash, -info['step']/2))
+            flash.draw()
+            did_flash = True
+            #print clock.getTime(), np.float(clock.getTime()/info['duration'])-.5, stim.pos
         win.flip()
-        while not(myMouse.isPressedIn(fixation)):#forever until we return mouse events
-            mouse1, mouse2, mouse3 = myMouse.getPressed()
-            if mouse3 or mouse2:
-                win.close()
-                core.quit()
-        pos_flash = info['pos_flash'] + (np.random.rand()-.5)*info['std_flash'] # a random number between 0 and 1
-        noise_flash = np.random.randn()*info['noise']
-        presentStimulus(pos_flash, noise_flash)
-        #wait_for_response.draw()
-        win.flip()
-        ans = getResponse()
-        print (ans)
-        results[0, i_trial] = ans[0]
-        results[1, i_trial] = ans[1]
-        results[2, i_trial] = pos_flash
-        results[3, i_trial] = noise_flash
 
-    win.update()
-    core.wait(0.5)
+#On va dessiner le stimuli et faire une mise a jour de la fenetre pour le nombre de nTrials.
+#Tant que la souris est visible pas de stimuli.
+#le flash rouge apparait pas toujours au centre il peut ce deplace a gauche ou a droite apres chaque trial.
+#On print les reponses du positionnement des flash rouges et des reponses choisis.
+results = np.zeros((4, info['nTrials']))
+for i_trial in range(info['nTrials']):
+    fixation.draw()
+    win.flip()
+    while not(myMouse.isPressedIn(fixation)):#forever until we return mouse events
+        mouse1, mouse2, mouse3 = myMouse.getPressed()
+    pos_flash = info['pos_flash'] + (np.random.rand()-.5)*info['std_flash'] # a random number between 0 and 1
+    noise_flash = np.random.randn()*info['noise']
+    presentStimulus(pos_flash, noise_flash)
+    #wait_for_response.draw()
+    win.flip()
+    ans = getResponse()
+    print (ans)
+    results[0, i_trial] = .5*ans[0]+.5
+    results[1, i_trial] = .5*ans[1]+.5
+    results[2, i_trial] = pos_flash
+    results[3, i_trial] = noise_flash
 
-    win.close()
+win.update()
+core.wait(0.5)
 
-    #save data
-    fileName = 'data/' + info['timeStr'] + '_' + experiment + '_' + info['observer']
-    np.save(fileName, results)
+win.close()
 
-print (fileName)
-import matplotlib.pyplot as plt
-fig, ax = plt.subplots(1, 1)
-ax.scatter(results[2, :], results[0, :], c='r')
-ax.scatter(results[2, :], results[1, :] + .3, c='b')
-ax.plot([info['pos_flash']-info['std_flash'], info['pos_flash']+info['std_flash']], [info['length']*(info['pos_flash']-info['std_flash']-.5), info['length']*(info['pos_flash']+info['std_flash']-.5)], 'g--')
-#pylab.axis([0., 1., -1.1, 1.1])
-ax.set_xlabel('position')
-error = results[0, :]/info['length']+.5-(results[2, :]-results[3, :])
-ax.text(info['pos_flash']-info['std_flash'], info['length']*(info['pos_flash']+info['std_flash']-.5), 'FLE = %0.2f ' % np.mean(error) + '+/- %0.2f ' %  np.std(error))
-fig.savefig('fle.pdf')
-print('Done')   
+#save data
+fileName = os.path.join(info['datapath'], info['timeStr'] + '_' + experiment + '_' + info['observer'] + '.npy')
+np.save(fileName, results)
---
+++ fle_analysis.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Flash_lag Effect
+
+
+
+
+"""
+#Sauvegarde les donnees et on fait un graphique des resultats
+import os
+from psychopy import misc
+
+#fileName = os.path.join(info['datapath'], info['timeStr'] + '_' + experiment + '_' + info['observer'] + '.pickle')
+fileName = os.path.join('data', '2016-03-29_163136_fle_anonymous')
+fileName = os.path.join('data', '2016-03-29_171633_fle_anonymous')
+fileName = os.path.join('data', 'debug_fle_anonymous')
+print (fileName)
+info = misc.fromFile(fileName + '.pickle')
+print(info)
+import numpy as np
+results = np.load(fileName + '.npy')
+print(results[0, :])
+
+import matplotlib.pyplot as plt
+fig, ax = plt.subplots(1, 1)
+ax.scatter(results[2, :], results[0, :], c='r', label=u'perçu')
+ax.scatter(results[2, :], results[2, :], c='b', marker='.',label=u'réel')
+#ax.plot([info['pos_flash']-info['std_flash'], info['pos_flash']+info['std_flash']], [info['length']*(info['pos_flash']-info['std_flash']-.5), info['length']*(info['pos_flash']+info['std_flash']-.5)], 'g--')
+ax.plot([info['pos_flash']-info['std_flash'], info['pos_flash']+info['std_flash']], [info['pos_flash']-info['std_flash'], info['pos_flash']+info['std_flash']], 'g--')
+#pylab.axis([0., 1., -1.1, 1.1])
+ax.set_xlabel(u'position du flash')
+ax.set_ylabel(u'position reportée / cliquée')
+ax.legend()
+error = results[0, :]-(results[2, :]-results[3, :])
+ax.text(.46, .6, 'FLE = %0.2f ' % np.mean(error) + '+/- %0.2f ' %  np.std(error))
+fig.savefig('fle.pdf')
+print('Done')
