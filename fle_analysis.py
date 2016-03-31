88b081276fa490ecd398aaceee2ed9bbd63afd2f
tony2693 <tony_wanping@hotmail.com>
2016-03-31 15:27:58 +0200

    plotting all results
    using glob to loop over files

--- fle_analysis.py
+++ fle_analysis.py
@@ -11,28 +11,33 @@ Flash_lag Effect
 import os
 from psychopy import misc
 
+datapath = 'data'
 #fileName = os.path.join(info['datapath'], info['timeStr'] + '_' + info['experiment'] + '_' + info['observer'] + '.pickle')
-fileName = os.path.join('data', '2016-03-29_163136_fle_anonymous')
-fileName = os.path.join('data', '2016-03-29_171633_fle_anonymous')
-fileName = os.path.join('data', 'debug_fle_anonymous')
-print (fileName)
-info = misc.fromFile(fileName + '.pickle')
-print(info)
-import numpy as np
-results = np.load(fileName + '.npy')
-print(results[0, :])
+#fileName = os.path.join('data', '2016-03-29_163136_fle_anonymous')
+#fileName = os.path.join('data', '2016-03-29_171633_fle_anonymous')
+#fileName = os.path.join('data', 'debug_fle_sujet 1')
+#fileName = os.path.join('data', 'debug_fle_anonymous')
+import glob
+for fileName in glob.glob(os.path.join(datapath, '*fle*.npy')):
+    print (fileName)
 
-import matplotlib.pyplot as plt
-fig, ax = plt.subplots(1, 1)
-ax.scatter(results[2, :], results[0, :], c='r', label=u'perçu')
-ax.scatter(results[2, :], results[2, :], c='b', marker='.',label=u'réel')
-#ax.plot([info['pos_flash']-info['std_flash'], info['pos_flash']+info['std_flash']], [info['length']*(info['pos_flash']-info['std_flash']-.5), info['length']*(info['pos_flash']+info['std_flash']-.5)], 'g--')
-ax.plot([info['pos_flash']-info['std_flash'], info['pos_flash']+info['std_flash']], [info['pos_flash']-info['std_flash'], info['pos_flash']+info['std_flash']], 'g--')
-#pylab.axis([0., 1., -1.1, 1.1])
-ax.set_xlabel(u'position du flash') 
-ax.set_ylabel(u'position reportée / cliquée')
-ax.legend()
-error = results[0, :]-(results[2, :]-results[3, :])
-ax.text(.46, .6, 'FLE = %0.2f ' % np.mean(error) + '+/- %0.2f ' %  np.std(error))
-fig.savefig('fle.pdf')
+    info = misc.fromFile(fileName.replace('.npy', '.pickle'))
+    print(info)
+    import numpy as np
+    results = np.load(fileName)
+    #print(results[0, :])
+
+    import matplotlib.pyplot as plt
+    fig, ax = plt.subplots(1, 1)
+    ax.scatter(results[2, :], results[0, :], c='r', label=u'perçu')
+    ax.scatter(results[2, :], results[2, :], c='b', marker='.',label=u'réel')
+    #ax.plot([info['pos_flash']-info['std_flash'], info['pos_flash']+info['std_flash']], [info['length']*(info['pos_flash']-info['std_flash']-.5), info['length']*(info['pos_flash']+info['std_flash']-.5)], 'g--')
+    ax.plot([info['pos_flash']-info['std_flash'], info['pos_flash']+info['std_flash']], [info['pos_flash']-info['std_flash'], info['pos_flash']+info['std_flash']], 'g--')
+    #pylab.axis([0., 1., -1.1, 1.1])
+    ax.set_xlabel(u'position du flash') 
+    ax.set_ylabel(u'position reportée / cliquée')
+    ax.legend()
+    error = results[0, :]-(results[2, :]-results[3, :])
+    ax.text(.46, .6, 'FLE = %0.2f ' % np.mean(error) + '+/- %0.2f ' %  np.std(error))
+    fig.savefig(fileName.replace('.npy', '.pdf'))
 print('Done')
